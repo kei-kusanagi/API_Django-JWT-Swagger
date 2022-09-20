2a7dec51673419b43b7d6dfc019c9d0a91e63c53
kei_kusanagi_99 <kei_kusanagi@outlook.com>
2022-09-19 19:09:42 -0500

    add loggers in settings.py

--- app/settings.py
+++ app/settings.py
@@ -159,3 +159,17 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
     'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
